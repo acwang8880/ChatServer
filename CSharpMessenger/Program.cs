e22188abf69158aed1d049fcf3addb4691353d98
Alex Wang <alexwang8880@gmail.com>
2016-06-30 17:44:20 -0700

    Working on multithreading Server: check Client: working on it

--- CSharpMessenger/Program.cs
+++ CSharpMessenger/Program.cs
@@ -1,20 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Net.Sockets;
 
 namespace CSharpMessenger
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        public static Hashtable clientsList = new Hashtable();
 
+        static void Main(string[] args)
+        { 
             //our server
             TcpListener serverSocketListener = new TcpListener(8888);
-            int requestCount = 0;
+            int counter = 0;
 
             //our client
             TcpClient clientSocket = default(TcpClient);
@@ -22,12 +23,95 @@ namespace CSharpMessenger
             //initiate server
             serverSocketListener.Start();
             Console.WriteLine("  >> Server initiated");
+            counter = 0;
+            
+            while (true)
+            {
+                counter += 1;
 
-            //check if client connected to server
-            clientSocket = serverSocketListener.AcceptTcpClient();
-            Console.WriteLine("  >> Accepted connection from client");
-            requestCount = 0;
+                //check if client connected to server
+                clientSocket = serverSocketListener.AcceptTcpClient();
+                Console.WriteLine("  >> " + "Client No: " + Convert.ToString(counter) + " connection accepted.");
 
+                //create buffer for storing client info
+                byte[] bytesFrom = new byte[65537];
+                string dataFromClient = null;
+
+                NetworkStream networkStream = clientSocket.GetStream();
+
+                //get data from network stream
+                networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+
+                //interpret client data
+                dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+
+                //create hashtable of clients
+                clientsList.Add(dataFromClient, clientSocket);
+                
+                //broadcast to all clients
+                broadcast(dataFromClient + " Joined ", dataFromClient, false);
+
+                Console.WriteLine(dataFromClient + " Joined ", dataFromClient);
+                handleClient client = new handleClient();
+                client.startClient(clientSocket, dataFromClient, clientsList);
+            }
+            //clean up everything
+            clientSocket.Close();
+            serverSocketListener.Stop();
+            Console.WriteLine("  >> Exiting client and server");
+            Console.ReadLine();
+        }
+
+        //broadcast server's messages to everyone on the server
+        public static void broadcast(string message, string username, bool flag)
+        {
+            foreach (DictionaryEntry item in clientsList)
+            {
+                TcpClient broadcastSocket;
+                broadcastSocket = (TcpClient) item.Value;
+                NetworkStream broadcastStream = broadcastSocket.GetStream();
+                Byte[] broadcastBytes = null;
+
+                //first time sending message?
+                if (flag == true)
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(username + " :" + message);
+                }
+                else
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(message);
+                }
+
+                broadcastStream.Write(broadcastBytes, 0, broadcastBytes.Length);
+                broadcastStream.Flush();
+            }
+        } //end broadcast
+    } // end Main class
+
+    public class handleClient
+    {
+        TcpClient clientSocket;
+        string clientNum;
+        Hashtable listOfClients;
+        
+        public void startClient(TcpClient clientIn, string num, Hashtable allClients)
+        {
+            this.clientSocket = clientIn;
+            this.clientNum = num;
+            this.listOfClients = allClients;
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+        private void doChat()
+        {
+            int requestCount = 0;
+            byte[] bytesFrom = new byte[65537];
+            string dataFromClient = null;
+            Byte[] sendBytes = null;
+            string serverResponse = null;
+            string requestCountString = null;
+            //requestCount = 0;
 
             while (true)
             {
@@ -37,21 +121,20 @@ namespace CSharpMessenger
                     NetworkStream networkStream = clientSocket.GetStream();
 
                     //get data from network stream
-                    byte[] bytesFrom = new byte[65537];
-                    Console.WriteLine("Buffer size: " + clientSocket.ReceiveBufferSize);
                     networkStream.Read(bytesFrom, 0, (int) clientSocket.ReceiveBufferSize);
-                    
 
                     //interpret client data
-                    string dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = Encoding.ASCII.GetString(bytesFrom);
                     dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
 
                     //display client data
-                    Console.WriteLine("  >> Data recieved from Client - " + dataFromClient);
-                    string serverResponse = "Last Message from Client " + dataFromClient;
+                    Console.WriteLine("  >> Data recieved from Client " + clientNum + ": " + dataFromClient);
+                    requestCountString = Convert.ToString(requestCount);
+
+                    Program.broadcast(dataFromClient, clientNum, true);
 
                     //write to network stream and clean up
-                    Byte[] sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+                    sendBytes = Encoding.ASCII.GetBytes(serverResponse);
                     networkStream.Write(sendBytes, 0, sendBytes.Length);
                     networkStream.Flush();
 
@@ -60,14 +143,9 @@ namespace CSharpMessenger
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e.ToString());
+                    Console.WriteLine("  >> " + e.ToString());
                 }
             }
-            //clean up everything
-            clientSocket.Close();
-            serverSocketListener.Stop();
-            Console.WriteLine("  >> Exiting client and server");
-            Console.ReadLine();
         }
     }
 }
--- ChatClient/Form1.cs
+++ ChatClient/Form1.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -14,7 +14,8 @@ namespace ChatClient
     public partial class Form1 : Form
     {
         TcpClient clientSocket = new TcpClient();
-
+        NetworkStream serverStream = default(NetworkStream);
+        string readData = null;
         // private string serverStatus = "Not connected ...";
         private string serverName;
 
@@ -23,19 +24,37 @@ namespace ChatClient
         {
             InitializeComponent();
             this.outputServerStatus.Text = "Client Socket Program - ";
+            ToolTip tooltip1 = new ToolTip();
+            // Set up the delays for the ToolTip.
+            tooltip1.AutoPopDelay = 5000;
+            tooltip1.InitialDelay = 1000;
+            tooltip1.ReshowDelay = 10;
+            // Force the ToolTip text to be displayed whether or not the form is active.
+            tooltip1.ShowAlways = true;
+            tooltip1.SetToolTip(this.inputServerName, "Press ENTER to confirm");
+
+
+//-->>Delete after Debugging completed            
+            this.inputServerName.Text = "127.0.0.1";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            msg("Client Started");
-
         }
 
-        private void msg(string v)
+        private void msg()
         {
-            outputChatDispalyBox.Text = outputChatDispalyBox.Text + Environment.NewLine + " >> " + v;
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(msg));
+            }
+            else
+            {
+                outputChatDispalyBox.Text = outputChatDispalyBox.Text + Environment.NewLine + " >> " + readData;
+            }
         }
 
+        //assigns server name
         private void inputServerName_Leave(object sender, EventArgs e)
         {
             if (inputServerName.Text.Equals(""))
@@ -46,13 +65,13 @@ namespace ChatClient
             else
             {
                 serverName = inputServerName.Text;
-                MessageBox.Show("Servername: " + serverName);
             }
 
             //MessageBox.Show("sender: " + sender.ToString() + "\n" + "e: " + e.ToString());
             
         }
 
+        //entering Server Name
         private void inputServerName_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
@@ -62,6 +81,16 @@ namespace ChatClient
                     inputServerName_Leave(inputServerName, e);
                     clientSocket.Connect(serverName, 8888);
                     outputServerStatus.Text += "Server Connected ...";
+                    serverStream = clientSocket.GetStream();
+
+                    //read from server
+                    byte[] inStream = new byte[65537];
+                    serverStream.Read(inStream, 0, (int) clientSocket.ReceiveBufferSize);
+
+                    //display in chat window
+                    string returndata = Encoding.ASCII.GetString(inStream);
+                    readData = "" + returndata;
+                    msg();
                 }
                 catch (Exception s)
                 {
@@ -72,22 +101,14 @@ namespace ChatClient
 
         private void inputSendButton_Click(object sender, EventArgs e)
         {
-            NetworkStream serverStream = clientSocket.GetStream();
-
+            readData = "Connected to Server ...";
+            msg();
             //convert input String to something server can understand
             byte[] outStream = Encoding.ASCII.GetBytes(inputClient.Text + "$");
 
             //give the server the good stuff
             serverStream.Write(outStream, 0, outStream.Length);
             serverStream.Flush();
-
-            //read from server
-            byte[] inStream = new byte[65537];
-            serverStream.Read(inStream, 0, (int) clientSocket.ReceiveBufferSize);
-
-            //display in chat window
-            string returndata = Encoding.ASCII.GetString(inStream);
-            msg(returndata);
 
             //clean input box
             inputClient.Text = "";
